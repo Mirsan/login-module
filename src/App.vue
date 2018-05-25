2291daf6ada1f70887377abca8b06204b8a71ec1
Mirsan <Mirsan7@gmail.com>
2018-05-25 22:48:21 +0200

    Add vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
   <div id="app">
+    <v-app id="inspire">
+    </v-app>
     <router-view/>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+
+Vue.use(Vuetify)
+
 export default {
   name: 'App'
 }
