561d0ce99dbf91df9710b92adb7abcb12e9ba717
Mirsan <Mirsan7@gmail.com>
2018-05-26 01:26:57 +0200

    Add axios

---
+++ src/router/router.js
@@ -0,0 +1,40 @@
+import store from '../store' // your vuex store
+
+const ifNotAuthenticated = (to, from, next) => {
+  if (!store.getters.isAuthenticated) {
+    next()
+    return
+  }
+  next('/')
+}
+
+const ifAuthenticated = (to, from, next) => {
+  if (store.getters.isAuthenticated) {
+    next()
+    return
+  }
+  next('/login')
+}
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home,
+    },
+    {
+      path: '/account',
+      name: 'Account',
+      component: Account,
+      beforeEnter: ifAuthenticated,
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login,
+      beforeEnter: ifNotAuthenticated,
+    },
+  ],
+})
